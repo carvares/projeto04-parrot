ac97be82465c4b052624dc0a611f54c4ad4b4cd7
carvares <lucas_tavares@id.uff.br>
2021-04-06 22:48:38 -0300

    7 opções de cartas incluidas

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 ndecartas = prompt("com quantas cartas quer jogar?");
+const todasAsCartas = ["bobrossparrot.gif", "explodyparrot.gif","fiestaparrot.gif","metalparrot.gif","revertitparrot.gif","tripletsparrot.gif","unicornparrot.gif"];
+const nDeGifs = ndecartas / 2;
+const codigocartas = [];
+let codigopronto = "<!--...-->";
+
 while (ndecartas % 2 != 0 || ndecartas == 0){
     ndecartas = prompt("com quantas cartas quer jogar?");
 }
-alert(ndecartas);
+
+
+    const cartas = document.querySelector(".cartas");
+    for (i = 0; i < nDeGifs; i++ ){
+        let carta = "img/" +todasAsCartas[i];
+
+            
+    
+        const conteudoCartas = `
+        <div class="carta"  "selecionada${i}">
+        <img src="${carta}" alt="papagaio">
+
+        </div>
+        `;
+        codigocartas[i] = conteudoCartas;
+    
+        
+        codigopronto = codigopronto + codigocartas[i];
+    }
+    cartas.innerHTML = codigopronto;
+    alert(codigocartas);
+   
+
+
+
+
+
+
+
+
+
+
+function virarcarta(qualcarta){
+
+
+}
